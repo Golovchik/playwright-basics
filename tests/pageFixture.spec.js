5e97b2d6158f3a076f01527b2819a82063cb9aec
Golovchik <golo.vv@yandex.ru>
2023-08-13 15:07:46 +0300

    M5: Fixtures

---
+++ tests/pageFixture.spec.js
@@ -0,0 +1,15 @@
+import { test as base, expect } from "@playwright/test";
+
+const test = base.extend({
+  page: async ({ page }, use) => {
+    await page.goto("https://www.epam.com");
+    await page.locator("#onetrust-accept-btn-handler").click();
+    await use(page);
+  },
+});
+
+test("open epam.com", async ({ page }) => {
+  await expect(page).toHaveTitle(
+    "EPAM | Software Engineering & Product Development Services",
+  );
+});
